92602a2b010ed452f51c3ab5a2fa299e1fffadf3
fernandoJoseC <fernandocalderon05@gmail.com>
2022-02-01 21:42:04 -0500

    subo cambios a heroku de CreateSeries

--- src/CreateSeries.js
+++ src/CreateSeries.js
@@ -1,50 +1,99 @@
 import React from 'react'
-import { Button, Checkbox, Form} from 'semantic-ui-react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 import InputExample from './inputHTML';
 import dataList from './dataList';
+import axios from 'axios';
+import { useState } from 'react/cjs/react.development';
+var qs = require('qs');
 
-const CreateSeries = () => (
+export default function CreateSeries() {
+    const [nombre, setNombre] = useState('');
+    const [numTemporadas, setNumTemporadas] = useState('');
+    const [director, setDirector] = useState('');
+    const [idioma, setIdioma] = useState('');
+    const [anioEstreno, setAnioEstreno] = useState('');
+    const [descripcion, setDescripcion] = useState('');
 
-    <Form>
-        <Form.Field>
-            <label>Nombre de la serie</label>
-            <br></br>
-            <input placeholder='Serie'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>Numero de temporadas</label>
-            <br></br>
-            <input placeholder='Temporadas'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>Director</label>
-            <br></br>
-            <input placeholder='Director'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>Idioma</label>
-            <br></br>
-            <input placeholder='Idioma'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>Año de estreno</label>
-            <br></br>
-            <input type="date" placeholder='Año'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>Descripcion</label>
-            <br></br>
-            <input placeholder='Descripcion'></input>
-        </Form.Field>
-        <Form.Field>
-        <label>URL de la serie</label>
-            <br></br>
-            <InputExample></InputExample>
-        </Form.Field>
-        <br />
-        <Button>Guardar</Button>
-    </Form>
+    const enviarDatos = () => {
+        console.log(nombre);
+        console.log(numTemporadas);
+        console.log(director);
+        console.log(idioma);
+        console.log(anioEstreno);
+        console.log(descripcion);
 
-)
+        var data = qs.stringify({
+            'nombre': nombre,
+            'numTemporadas': director,
+            'director': director,
+            'idioma': idioma,
+            'anioEstreno': anioEstreno,
+            'descripcion': descripcion
+        });
 
-export default CreateSeries;
+        var config = {
+            method: 'post',
+            url: 'https://crudmongofernando.herokuapp.com/series/crearSerie',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                window.alert("serie guardada")
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                window.alert("ocurrio un error")
+                console.log(error);
+            });
+
+    }
+
+    return (
+
+        <Form>
+            <Form.Field>
+                <label>Nombre de la serie</label>
+                <br></br>
+                <input placeholder='Serie' onChange={(e) => setNombre(e.target.value)}></input>
+            </Form.Field>
+            <Form.Field>
+                <label>Numero de temporadas</label>
+                <br></br>
+                <input placeholder='Temporadas' onChange={(e) => setNumTemporadas(e.target.value)}></input>
+            </Form.Field>
+            <Form.Field>
+                <label>Director</label>
+                <br></br>
+                <input placeholder='Director' onChange={(e) => setDirector(e.target.value)}></input>
+            </Form.Field>
+            <Form.Field>
+                <label>Idioma</label>
+                <br></br>
+                <input placeholder='Idioma' onChange={(e) => setIdioma(e.target.value)}></input>
+            </Form.Field>
+            <Form.Field>
+                <label>Año de estreno</label>
+                <br></br>
+                <input type="number" placeholder='Anio' onChange={(e) => setAnioEstreno(e.target.value)}></input>
+            </Form.Field>
+            <Form.Field>
+                <label>Descripcion</label>
+                <br></br>
+                <input placeholder='Descripcion' onChange={(e) => setDescripcion(e.target.value)}></input>
+            </Form.Field>
+            
+            <br />
+            <Button onClick={enviarDatos} type="submit">Guardar</Button>
+        </Form>
+
+    )
+
+
+
+
+}
+
